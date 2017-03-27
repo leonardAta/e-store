69e26f39b4e2237cdc9cda94b70d45db0d071352
Ata Tangban <atatangban@live.com>
2017-03-27 16:07:09 +0100

    home page structure

---
+++ www/home.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<div id="container">
+		<div id="nav">
+			<a href="">SHOP</a>
+			<a href="">ABOUT</a>
+			<a href="">BLOG</a>
+			<a href="">CONTACT</a>
+		</div> <!-- end of nav div-->
+
+		<div id="cover-img">
+		</div> <!-- end of cover-img div-->
+		<hr/>
+		<br/>
+
+		<div id="body">
+		</div> <!-- end of body div-->
+	</div> <!-- end of container div-->
+</body>
+</html>
